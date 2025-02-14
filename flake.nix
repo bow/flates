827e18050b0bbb86b3d947caaddaa6557fcb6633
Wibowo Arindrarto <contact@arindrarto.dev>
2025-02-14 06:17:59 +0100

    chore: Add missing top-level flake for local dev

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = ''
+    Nix flake templates.
+
+    Usage: `nix flake {new,init} -t github:bow/flates#<template-name> [<dir>]`
+  '';
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }: let
+    env = flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+        nixtools = with pkgs; [alejandra deadnix statix];
+      in {
+        devShells.default = pkgs.mkShellNoCC {packages = nixtools;};
+        formatter = pkgs.alejandra;
+      }
+    );
+  in
+    env
+    // {
+      templates = rec {
+        default = base;
+        base = {
+          path = ./base;
+          description = "Base template";
+        };
+      };
+    };
+}
