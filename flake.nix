265799c4d674665fcadd933a8f7f5a5d139d492b
Wibowo Arindrarto <contact@arindrarto.dev>
2025-02-18 21:11:38 +0100

    chore: Add helper commands to flake.nix

--- flake.nix
+++ flake.nix
@@ -21,18 +21,68 @@
         description = (import ./${name}/flake.nix).description;
       };
     };
+    templates =
+      template "asciidoc"
+      // template "default";
   in
-    {
-      templates =
-        template "asciidoc"
-        // template "default";
-    }
+    {templates = templates;}
     // flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = import nixpkgs {inherit system;};
         nixTools = with pkgs; [alejandra deadnix statix];
+        # Adapted from https://github.com/the-nix-way/dev-templates
+        forEachDir = cmd: ''
+          for dir in ${builtins.concatStringsSep " " (builtins.attrNames templates)}; do
+            (
+              cd "''${dir}"
+              ${cmd}
+            )
+          done
+        '';
+        localTools = [
+          (pkgs.writeShellApplication {
+            name = "build";
+            runtimeInputs = [pkgs.alejandra];
+            text = ''
+              ${forEachDir ''
+                echo "→ building ''${dir}"
+                nix build ".#devShells.${system}.default"
+              ''}
+            '';
+          })
+          (pkgs.writeShellApplication {
+            name = "check";
+            text = forEachDir ''
+              echo "→ checking ''${dir}"
+              nix flake check --all-systems --no-build
+            '';
+          })
+          (pkgs.writeShellApplication {
+            name = "clean";
+            text = forEachDir ''
+              echo "→ cleaning ''${dir}"
+              rm -f result
+            '';
+          })
+          (pkgs.writeShellApplication {
+            name = "format";
+            runtimeInputs = [pkgs.alejandra];
+            text = ''
+              shopt -s globstar
+
+              alejandra -- **/*.nix
+            '';
+          })
+          (pkgs.writeShellApplication {
+            name = "update";
+            text = forEachDir ''
+              echo "→ updating ''${dir}"
+              nix flake update
+            '';
+          })
+        ];
       in {
-        devShells.default = pkgs.mkShellNoCC {packages = nixTools;};
+        devShells.default = pkgs.mkShellNoCC {packages = localTools ++ nixTools;};
         formatter = pkgs.alejandra;
       }
     );
