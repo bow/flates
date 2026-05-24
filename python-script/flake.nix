ff77d9bfbeaf20d33015828308627da61c4ee2d5
Wibowo Arindrarto <contact@arindrarto.dev>
2026-05-24 22:58:39 +0200

    python-script: Remove nixpkgs-python and use nixpkgs

--- python-script/flake.nix
+++ python-script/flake.nix
@@ -4,29 +4,24 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs-python = {
-      url = "github:cachix/nixpkgs-python";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs =
     {
       nixpkgs,
       flake-utils,
-      nixpkgs-python,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = import nixpkgs { inherit system; };
-        pyVersion = "3.13";
+        pkgs = nixpkgs.legacyPackages.${system};
+        pyPackage = pkgs.python314;
         pyTools = with pkgs; [
           black
           ruff
         ];
-        python = nixpkgs-python.packages.${system}.${pyVersion}.withPackages (p: [ p.mypy ]);
+        python = pyPackage.withPackages (p: [ ]);
         devPkgs = [ python ];
         nixTools = with pkgs; [
           deadnix
